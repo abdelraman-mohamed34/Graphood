a516edf136707e58e2967b14a0b6ee3bdd05f35c
abdelraman-mohamed34 <abdelraman.mohamed34@gmail.com>
2026-08-05 04:08:06 +0300

    style(dashbaord): update responsive Go Live component

--- src/app/[locale]/(dashboard)/(tenant)/[tenant_slug]/dashboard/quickview/_components/go-live.tsx
+++ src/app/[locale]/(dashboard)/(tenant)/[tenant_slug]/dashboard/quickview/_components/go-live.tsx
@@ -12,15 +12,15 @@ export default function GoLive() {
     const destinationUrl = `https://${tenant}.graphood.com`
 
     return (
-        <div className="w-full py-2  px-5 bg-amber-200/50">
-            <div className="flex flex-col md:flex-row items-center justify-between gap-4">
+        <div className="w-full py-2 px-5 bg-amber-200/50">
+            <div className="flex flex-row items-center justify-between gap-4">
                 <div>
                     <h3 className="text font-semibold tracking-tight text-foreground">
                         {t('launch.title')}
                     </h3>
                 </div>
 
-                <Link href={tenant ? destinationUrl : '/not-found'} target="_blank" rel="noopener noreferrer" className="w-full md:w-auto">
+                <Link href={tenant ? destinationUrl : '/not-found'} target="_blank" rel="noopener noreferrer">
                     <Button className="w-full md:w-auto gap-2" size="lg">
                         {t('launch.action')}
                         <ExternalLink className="h-4 w-4" />
