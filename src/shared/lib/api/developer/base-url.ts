eca554a5dfe462d682159958aee78774f2519c1a
abdelraman-mohamed34 <abdelraman.mohamed34@gmail.com>
2026-08-19 00:47:43 +0300

    feat(docs): update domain

--- src/shared/lib/api/developer/base-url.ts
+++ src/shared/lib/api/developer/base-url.ts
@@ -1,2 +1,2 @@
-export const DEVELOPER_API_BASE_URL = "https://graphood-5x58.vercel.app/api/developer/v1";
+export const DEVELOPER_API_BASE_URL = "https://www.graphood.com/api/developer/v1";
 export const DEVELOPER_SANDBOX_API_BASE_URL = DEVELOPER_API_BASE_URL;
